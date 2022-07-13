ccc3946824f275db653d3c6f11a6b7e4706c9a06
nikkolet <nikkoletashby@Gmail.com>
2022-07-13 01:13:16 -0600

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import arcade
+from matplotlib.pyplot import title
 from constants import *
 from game.directing.director import Director
 
@@ -7,7 +8,7 @@ def main():
     This will initiate the game
     """
     # Creates the game and starts it going
-    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, title = "Attack the Death Star")
     director = Director(window)
     director.start_game()
 
