340cd764c3f95a43ea826f96e38393bafe52bafa
nikkolet <nikkoletashby@Gmail.com>
2022-07-13 02:24:10 -0600

    commit

--- __main__.py
+++ __main__.py
@@ -1,7 +1,9 @@
 import arcade
 from matplotlib.pyplot import title
+from pyray import set_window_icon
 from constants import *
 from game.directing.director import Director
+from pyglet.image import load as pyglet_load
 
 def main():
     """
@@ -9,6 +11,10 @@ def main():
     """
     # Creates the game and starts it going
     window = arcade.Window(SCREEN_WIDTH, SCREEN_HEIGHT, title = "Attack the Death Star")
+    window.set_icon(pyglet_load("assets/images/favicon.ico"))
+    # "assets/images/favicon.ico"
+   
+    #assets/images/favicon.ico
     director = Director(window)
     director.start_game()
 
